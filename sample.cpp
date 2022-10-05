d7295c45b0d1bac441047963f4d3a46458669b1f
Vincent0913 <b5851317@gmail.com>
2022-10-05 17:42:17 +0800

    Sample

---
+++ sample.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define INF 0x3f3f3f3f
+#define ff first
+#define ss second 
+
+void solve()
+{
+	
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	solve();
+	return 0;	
+} 
